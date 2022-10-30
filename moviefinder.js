424b1d75581b36365daa428fa67772e62b082530
Alberto Duarte <betodute@gmail.com>
2022-10-29 19:12:38 -0700

    sntax error corrected
    className not class

--- moviefinder.js
+++ moviefinder.js
@@ -1,5 +1,7 @@
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -111,7 +113,7 @@ var MovieFinder = function (_React$Component) {
               }),
               React.createElement(
                 'button',
-                { 'class': 'p-2', type: 'submit', className: 'btn btn-primary' },
+                _defineProperty({ className: 'p-2', type: 'submit' }, 'className', 'btn btn-primary'),
                 'Submit'
               )
             ),
--- src/moviefinder.js
+++ src/moviefinder.js
@@ -71,7 +71,7 @@ class MovieFinder extends React.Component {
                 value={searchTerm}
                 onChange={this.handleChange}
               />
-              <button class="p-2" type="submit" className="btn btn-primary">Submit</button>
+              <button className="p-2" type="submit" className="btn btn-primary">Submit</button>
             </form>
             {(() => {
               if (error) {
